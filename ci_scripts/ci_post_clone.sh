9f24f3993ee153ade2fa890608818f4f5af11b38
Yasser Farahi <bookend.windows-59@icloud.com>
2025-03-18 12:25:07 +0100

    added xcode cloud stuff and localizations

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Set the -e flag to stop running the script in case a command returns
+# a non-zero exit code.
+set -e
+
+# Allow usage of macros
+defaults write com.apple.dt.Xcode IDESkipMacroFingerprintValidation -bool YES
+
+# Allow usage of plugins
+defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
